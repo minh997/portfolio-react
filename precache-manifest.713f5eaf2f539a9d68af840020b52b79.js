68b37972e201ee187dabbe2339a2cdbd1a34e62d
minh997 <39399443+minh997@users.noreply.github.com>
2019-06-19 21:54:14 -0700

    Updates

---
+++ precache-manifest.713f5eaf2f539a9d68af840020b52b79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7fe4c1dd883de328436435f48c09e07",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "7b9891c0845a6843eac2",
+    "url": "/portfolio-react/static/css/main.78d3ce90.chunk.css"
+  },
+  {
+    "revision": "7c0c63d6bf7414ccccb6",
+    "url": "/portfolio-react/static/js/2.e0ecda74.chunk.js"
+  },
+  {
+    "revision": "7b9891c0845a6843eac2",
+    "url": "/portfolio-react/static/js/main.25ff7d7d.chunk.js"
+  },
+  {
+    "revision": "11528ddda4154ce1e32b",
+    "url": "/portfolio-react/static/js/runtime~main.72b3206a.js"
+  },
+  {
+    "revision": "789a8ec3d08ca6fdbd1bb0cef00c8b8f",
+    "url": "/portfolio-react/static/media/test.789a8ec3.png"
+  }
+]);
