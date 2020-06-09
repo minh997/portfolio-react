b5ecec326a1d524bef57aec94cb34986fd3c5f81
Minh Nguyen <hoangminh160997@gmail.com>
2020-06-09 14:25:05 -0700

    style update

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,20 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Section extends Component {
-    render() {
-        return (
-            <div className="section">
-                <h1>{this.props.title}</h1>
+const Section = (props) => {
+    return (
+        <div className="section">
+            <h1>{props.title}</h1>
 
-                <div>{this.props.content}</div>
-            </div>
-        );
-    }
-}
-
-Section.defaultProps = {
-    title: "title",
-    content: "ABALNDLADNLSDNDnalshflasnpehq;f ajsf;ansf;ajasd;a",
+            <div>{props.content}</div>
+        </div>
+    );
 };
 
 export default Section;
