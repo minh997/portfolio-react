e58aa46b92b4f74c87a9b25cd6bd45add3faab65
Minh Nguyen <hoangminh160997@gmail.com>
2020-06-15 19:55:04 -0700

    style pdate

--- src/components/ProjectSection/Project.js
+++ src/components/ProjectSection/Project.js
@@ -5,7 +5,7 @@ import "./Project.css";
 const Project = (props) => {
     return (
         <div className="slide">
-            <div className="card neumorphism-1 p-3">
+            <div className="card neumorphism-1 p-1">
                 <video
                     loop
                     controls
